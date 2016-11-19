5fb5652a0f0d5d83e277adc3bf79baadff108169
rneha725 <rneha725@gmail.com>
2016-11-19 23:59:33 +0530

    TLS basic code

--- index.js
+++ index.js
@@ -3,10 +3,19 @@ var http = require('http');
 var url = require('url');
 var path = require('path');
 var NLP = require('stanford-corenlp');
+var AIMLInterpreter = require('./node_modules/aimlinterpreter');
 
 var app = express();
+var aimlInterpreter = new AIMLInterpreter({name:'BOTInterpreter', age:'0'});
 const MAIN_PORT = 2000;
 const NLP_PORT = 8990;
+
+aimlInterpreter.loadAIMLFilesIntoArray(['./aiml_files/test.aiml.xml']);//
+
+var callback = function(answer, wildCardArray, input){
+	console.log(answer + ' | ' + wildCardArray + ' | ' + input);
+
+};
 
 //For post request
 var bodyParser = require('body-parser');
@@ -43,18 +52,65 @@ app.post('/home', function(req, res) {
             tokenArray = body.document.sentences.sentence.tokens.token;
 
             var nounObject = new Object();
-
+            var number = '';
             for(var i = 0; i < tokenArray.length; i++) {
             	var wordToken = tokenArray[i];
 							console.log(wordToken);
 
 							if(wordToken.POS == "NN" || wordToken.POS == "NNS" || wordToken.POS == "NNP" || wordToken.POS == "NNPS" || wordToken.POS == "PRP" || wordToken.POS == "PRP$") {
 								nounObject[wordToken.word] = wordToken.POS;
-							}
 
+								if(wordToken.word == "semester" || wordToken.word == "sem") {
+									var j = i;
+									if((tokenArray[j+1].word).match(/\d+/) || tokenArray[j+1].word.match(/\d(?=th)/)) {
+										number = number + (tokenArray[j+1].word)[0];
+										console.log("Inside:" + number);
+									} 
+								}
+							}
 						}
+						console.log("Outside:" + number);
 
 						console.log(nounObject);
+						var code = "";
+						var flag = 1;
+
+						if(nounObject["syllabus"]!=null) {
+							code = code + 's';
+							flag = 0;
+						}
+						else if(nounObject["timetable"]!=null) {
+							code = code + 't';
+							flag = 0;
+						}
+						else if(nounObject["lesson"] && nounObject["plan"]) {
+							code = code + 'l';
+							flag = 0;
+						}
+
+						//for Degree after TLS
+						if(!flag && nounObject["btech"]) {
+							code = 'b' + code;
+						}
+						else if(!flag && nounObject["mtech"]) {
+							code = 'm' + code;
+						}
+
+						//for semester after TLS
+						if(!flag) {
+							code = number + code;
+						}
+
+						//for branch after TLS
+						if(!flag && nounObject["cse"]) {
+							code = 'c' + code;
+						}
+						
+						else if(!flag && nounObject["ece"]) {
+							code = 'e' + code;
+						}
+
+						aimlInterpreter.findAnswerInLoadedAIMLFiles(code, callback);
         }
         else console.log("ERROR in NLP Request.");
     })
